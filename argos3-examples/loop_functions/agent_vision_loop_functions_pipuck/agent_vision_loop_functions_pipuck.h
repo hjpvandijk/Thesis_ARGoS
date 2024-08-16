a15ec603096ab62fcb6eec544b860a0fa3c4e109
hjpvandijk <hugojpvandijk@gmail.com>
2024-08-16 18:10:02 +0200

    Added dynamic spawning objects using the user functions

--- argos3-examples/loop_functions/agent_vision_loop_functions_pipuck/agent_vision_loop_functions_pipuck.h
+++ argos3-examples/loop_functions/agent_vision_loop_functions_pipuck/agent_vision_loop_functions_pipuck.h
@@ -3,6 +3,8 @@
 
 #include <argos3/core/simulator/loop_functions.h>
 #include <argos3/plugins/robots/pi-puck/simulator/pipuck_entity.h>
+#include <argos3/plugins/simulator/entities/box_entity.h>
+
 #include "controllers/pipuck_hugo/pipuck_hugo.h"
 #include "agent_implementation/Quadtree.h"
 using namespace argos;
@@ -49,6 +51,21 @@ public:
     inline const std::map<CPiPuckEntity*, double>& GetAgentElapsedTicks() const {
         return m_tAgentElapsedTicks;
     }
+
+//    CBoxEntity* box = new CBoxEntity("new_box", CVector3(-2, 1, 0), CQuaternion(), false, CVector3(1.0, 1.0, 0.5), 0.0); ////        theMap.insert(std::make_pair("new_box", &box));
+
+    std::tuple<argos::CVector3, argos::CVector3, int> spawnableObjects[10] = {
+            std::make_tuple(argos::CVector3(-2, 1, 0), argos::CVector3(1.0, 1.0, 0.5), 10),
+            std::make_tuple(argos::CVector3(2, 1, 0), argos::CVector3(1.0, 1.0, 0.5), 20),
+            std::make_tuple(argos::CVector3(0, 1, 0), argos::CVector3(1.0, 1.0, 0.5), 30),
+            std::make_tuple(argos::CVector3(-2, -1, 0), argos::CVector3(1.0, 1.0, 0.5), 40),
+            std::make_tuple(argos::CVector3(2, -1, 0), argos::CVector3(1.0, 1.0, 0.5), 50),
+            std::make_tuple(argos::CVector3(0, -1, 0), argos::CVector3(1.0, 1.0, 0.5), 60),
+            std::make_tuple(argos::CVector3(-2, 0, 0), argos::CVector3(1.0, 1.0, 0.5), 70),
+            std::make_tuple(argos::CVector3(2, 0, 0), argos::CVector3(1.0, 1.0, 0.5), 80),
+            std::make_tuple(argos::CVector3(0, 0, 0), argos::CVector3(1.0, 1.0, 0.5), 90),
+            std::make_tuple(argos::CVector3(0, 0, 0), argos::CVector3(1.0, 1.0, 0.5), 100)
+    };
 
 private:
 
