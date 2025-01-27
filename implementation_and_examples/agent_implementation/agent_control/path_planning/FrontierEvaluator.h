93406a27d9642bc2687bda7c2e978abba1de06da
Hugo van Dijk <56676810+hjpvandijk@users.noreply.github.com>
2025-01-27 11:51:57 +0100

    Experiment start/end + bugfixes + blacklist update + cpu optimizations
    * WIP: mission start & en
    d
    
    * Now we end when we reach the deployment position
    
    * Replaced bresenhamline algorithm with Amanatides & Woo traversal algorithm
    
    * Bugfix angle comparison
    
    * Mission start and end working, bug fix skipping target
    
    * Raytracing updates
    
    * liang_barsky result check for intersection
    
    * When there are no free angles by checking obstacles and sensor collisions, re-add angles within 10 degrees around any sensor that has no obstacle detection, in hopes of getting out
    
    * Time sync atttempt now made once every interval. In case message doesn't arrive, syncing waits until next interval
    
    * Time sync cleanup'
    
    * Whenver a frontier is blacklisted, we blacklist the current route sub… (#150)
    
    * Whenver a frontier is blacklisted, we blacklist the current route subtarget too, and vice versa
    
    * Updated random walking, now we only poll new frontier if we have moved a bit. Also bug fix path following reach last
    
    * Max route length config for path planning, random walker bug fix, fro… (#151)
    
    * Max route length config for path planning, random walker bug fix, frontier seleciton criteria update
    
    * Random walk check if need to change because avoiding
    
    * Change route not empty assert to if
    
    * p threshold bug fix
    
    * p threshold fix
    
    * Experiment config update, and bug fix distance from others agents frontier comparison

--- implementation_and_examples/agent_implementation/agent_control/path_planning/FrontierEvaluator.h
+++ implementation_and_examples/agent_implementation/agent_control/path_planning/FrontierEvaluator.h
@@ -9,7 +9,7 @@ class Agent;
 
 class FrontierEvaluator {
 public:
-    std::vector<Coordinate> avoidingFrontiers;
+    std::vector<Coordinate> avoidingCoordinates;
 
     //If the agent is counts this many ticks without a direction to the target, it will skip the target
     int MAX_COUNT_NO_DIRECTION = 150;
@@ -22,7 +22,8 @@ public:
     FrontierEvaluator(int closest_coordinate_hit_count_before_decreasing_confidence, int max_ticks_no_direction);
 
     void resetFrontierAvoidance(Agent* agent, argos::CVector2 unexploredFrontierVector);
-    bool avoidingFrontier(Agent* agent);
+    bool avoidingAgentTarget(Agent* agent);
+    bool avoidingCoordinate(Agent* agent, Coordinate coordinate);
     void skipIfFrontierUnreachable(Agent* agent, argos::CRadians objectAvoidanceAngle, argos::CVector2 total_vector);
 
 private:
---
+++ implementation_and_examples/agent_implementation/agent_control/path_planning/RandomWalk.cpp
@@ -0,0 +1,61 @@
+
+#include "RandomWalk.h"
+#include "agent.h"
+
+
+/**
+ * When there is no target to be found within the search range, select a random location on the edge of the root box.
+ * @param targetVector
+ */
+void RandomWalk::randomWalk(Agent* agent, argos::CVector2 &targetVector) {
+    argos::CVector2 agentToSubtarget = argos::CVector2(agent->subTarget.x - agent->position.x,
+                                                       agent->subTarget.y - agent->position.y);;
+    if (!this->randomWalking
+        #ifdef DISALLOW_FRONTIER_SWITCHING_UNTIL_REACHED
+        //Or we have reached the subtarget (random walk target)
+        || agentToSubtarget.Length() <= agent->config.FRONTIER_DIST_UNTIL_REACHED
+        #endif
+        #ifdef SKIP_UNREACHABLE_FRONTIERS
+        //Or we are avoiding the subtarget, so we need to find a new one
+        || agent->frontierEvaluator.avoidingCoordinate(agent, agent->subTarget)
+        #endif
+            ) {
+        //Find a random direction to walk in, by placing a subtarget on the edge of the root box in the quadtree
+        quadtree::Box rootBox = agent->quadtree->getRootBox();
+        Coordinate rootBoxCenter = rootBox.getCenter();
+        double rootBoxSize = rootBox.getSize();
+        do{
+            argos::CRadians randomAngle = (rand() % 360) * argos::CRadians::PI /
+                                          180; //TODO: Maybe away from average location of other agents?
+            argos::CVector2 subtargetVector = argos::CVector2(1, 0);
+            subtargetVector.Rotate(randomAngle);
+            subtargetVector.Normalize();
+            subtargetVector *= rootBoxSize * 0.5;
+            agent->subTarget = {rootBoxCenter.x + subtargetVector.GetX(), rootBoxCenter.y + subtargetVector.GetY()};
+            agentToSubtarget = argos::CVector2(agent->subTarget.x - agent->position.x,
+                                               agent->subTarget.y - agent->position.y);
+            this->walkStart = agent->position;
+        } while (
+                #ifdef SKIP_UNREACHABLE_FRONTIERS
+                //If the subtarget is close to a frontier we are currently avoiding, try again
+                agent->frontierEvaluator.avoidingCoordinate(agent, agent->subTarget
+                #else
+                false
+                #endif
+                ));
+        this->randomWalking = true;
+
+
+    }
+    targetVector = agentToSubtarget;
+}
+
+bool RandomWalk::randomWalkedFarEnough(Agent *agent) {
+    //If the difference between walkStart and the agent is far enough, return true
+    if (sqrt(pow(agent->position.x - walkStart.x, 2) + pow(agent->position.y - walkStart.y, 2)) >= this->farEnoughDistance) {
+        this->walkStart = agent->position;
+        return true;
+    }
+    return false;
+}
+
