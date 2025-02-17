c552e51c9e108e3f1a563f14157a14b4b2a3c1f9
hjpvandijk <hugojpvandijk@gmail.com>
2025-02-17 22:22:39 +0100

    automatically run different number of agents from bash

--- implementation_and_examples/run_experiments.bash
+++ implementation_and_examples/run_experiments.bash
@@ -14,52 +14,68 @@ ARGOSEXEC="argos3"
 mkdir -p "$LOG_DIR"
 
 # List of experiment files (modify as needed)
-EXPERIMENTS=("museum_config.argos")
+EXPERIMENTS=("office.argos")
 CONFIGS=("config.yaml")
 
-
+N_AGENTS=6
 
 # Iterate over each experiment file
 for EXPERIMENT in "${EXPERIMENTS[@]}"; do
     EXP_PATH="$EXPERIMENT_DIR/$EXPERIMENT"
 #    export EXPERIMENT
 
+
     for CONFIG_FILE in "${CONFIGS[@]}"; do
         CONFIG_PATH="$CONFIG_DIR/$CONFIG_FILE"
 #        export CONFIG_FILE
 
-        METRIC_PATH="experiment_results/${EXPERIMENT%.argos}_${CONFIG_FILE%.yaml}"
-        export METRIC_PATH
-        FRAME_PATH="experiment_results/${EXPERIMENT%.argos}_${CONFIG_FILE%.yaml}/frames"
-
-        sed "s|{{CONFIG_PATH}}|$CONFIG_PATH|g" "$EXP_PATH" > temp_experiment.argos
-        sed "s|{{FRAME_PATH}}|${FRAME_PATH}|g" temp_experiment.argos > temp_experiment2.argos
-        mkdir -p "$FRAME_PATH"
-        # empty the directory
-        rm -rf "$FRAME_PATH"/*
-
-        # Check if the configuration file exists
-        if [ ! -f "$EXP_PATH" ]; then
-            echo "Error: Experiment config '$EXP_PATH' not found!" >&2
-            continue
-        fi
-
-        # Generate timestamp for log file
-        TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
-        LOGFILE="$LOG_DIR/${EXPERIMENT%.argos}_${CONFIG_FILE%.yaml}_$TIMESTAMP.log"
+        sed "s|{{CONFIG_PATH}}|${CONFIG_PATH}|g" "$EXP_PATH" > temp_experiment.argos
 
 
-        echo "Running ARGoS3 with configuration: $EXP_PATH and $CONFIG_PATH"
+        for remove_agents in $(seq 0 $((N_AGENTS-1))); do
 
-        # Run ARGoS3 and log output
-        "$ARGOSEXEC" -n -c "temp_experiment2.argos"> "$LOGFILE" 2>&1
 
-        # Check exit status
-        if [ $? -eq 0 ]; then
-            echo "Experiment '$EXPERIMENT' completed successfully."
-        else
-            echo "Experiment '$EXPERIMENT' failed. Check log: $LOGFILE" >&2
-        fi
+            METRIC_PATH="experiment_results/${EXPERIMENT%.argos}/${CONFIG_FILE%.yaml}/$((N_AGENTS-remove_agents))_agents"
+            export METRIC_PATH
+            FRAME_PATH="experiment_results/${EXPERIMENT%.argos}/${CONFIG_FILE%.yaml}/$((N_AGENTS-remove_agents))_agents/frames"
+            sed "s|{{FRAME_PATH}}|${FRAME_PATH}|g" temp_experiment.argos > temp_experiment2.argos
+
+
+            mkdir -p "$FRAME_PATH"
+            # empty the directory
+            rm -rf "$FRAME_PATH"/*
+
+            # Remove agents from the experiment file
+            if [ $remove_agents -ne 0 ]; then
+                for i in $(seq 1 $remove_agents); do
+                  sed -i "/<pipuck id=\"pipuck$i\"/,/<\/pipuck>/d" temp_experiment2.argos
+              done
+            fi
+
+            # Check if the configuration file exists
+            if [ ! -f "$EXP_PATH" ]; then
+                echo "Error: Experiment config '$EXP_PATH' not found!" >&2
+                continue
+            fi
+
+            # Generate timestamp for log file
+            TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+            LOGFILE="$LOG_DIR/${EXPERIMENT%.argos}_${CONFIG_FILE%.yaml}_$((N_AGENTS-remove_agents))}_$TIMESTAMP.log"
+
+            echo "Running ARGoS3 with configuration: $EXP_PATH and $CONFIG_PATH with $((N_AGENTS-remove_agents)) agents"
+
+
+
+            # Run ARGoS3 and log output
+            "$ARGOSEXEC" -n -c "temp_experiment2.argos"> "$LOGFILE" 2>&1
+
+            # Check exit status
+            if [ $? -eq 0 ]; then
+                echo "Experiment '$EXPERIMENT' with config '$CONFIG_FILE' and $((N_AGENTS-remove_agents)) agents finished successfully."
+            else
+                echo "Experiment '$EXPERIMENT' with config '$CONFIG_FILE' and $((N_AGENTS-remove_agents)) agents failed. Check log: $LOGFILE" >&2
+            fi
+      done
 
     done
 
