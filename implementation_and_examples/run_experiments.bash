6076873b90dcfd364a8db56b51265d4d46a2ba1f
hjpvandijk <hugojpvandijk@gmail.com>
2025-03-09 12:42:53 +0100

    Different seeds for repeated experiments

--- implementation_and_examples/run_experiments.bash
+++ implementation_and_examples/run_experiments.bash
@@ -15,7 +15,7 @@ mkdir -p "$LOG_DIR"
 
 # List of experiment files (modify as needed)
 #EXPERIMENTS=("house.argos" "house_tilted.argos" "office.argos" "office_tilted.argos" "museum.argos" "museum_tilted.argos")
-EXPERIMENTS=("house.argos" "house_tilted.argos")
+EXPERIMENTS=("house.argos")
 #CONFIGS=("config__alignment0_1__cohesion__0.yaml" "config__alignment0_1__cohesion__0_1.yaml" "config__alignment0__cohesion__0.yaml" "config__alignment0__cohesion__0_1.yaml")
 #CONFIGS=("config_bigger_safety_n_1.yaml" "config_bigger_safety_range.yaml" "config_bigger_safety_n_3.yaml")
 #CONFIGS=("n_3_m_2_5_cellratio0_75_noise.yaml" "n_3_m_2_5_cellratio0_75_noise_agent_avoidance_0_5.yaml" "n_3_m_2_5_cellratio0_75_noise_object_safety_0_3.yaml")
@@ -24,7 +24,7 @@ CONFIGS=("p_sensor_0_5_no_doubt.yaml")
 
 
 
-PARALLEL_JOBS=6
+PARALLEL_JOBS=5
 declare -A pids  # Associative array to store PIDs and their related info
 
 N_AGENTS=15
@@ -49,7 +49,7 @@ for r in $(seq 1 $((N_REPEATED_EXPERIMENTS))); do
           CONFIG_PATH="$CONFIG_DIR/$CONFIG_FILE"
   #        export CONFIG_FILE
 
-          sed "s|{{CONFIG_PATH}}|${CONFIG_PATH}|g" "$EXP_PATH" > "temp_${CONFIG_FILE%.yaml}_${EXPERIMENT}"
+          sed "s|{{CONFIG_PATH}}|${CONFIG_PATH}|g" "$EXP_PATH" > "temp_${CONFIG_FILE%.yaml}_${EXPERIMENT}_S${SEED}"
 
           for i in $(seq 1 $((N_AGENTS))); do
             ORIENTATION=$(shuf -i 0-360 -n 1 --random-source=<(openssl enc -aes-256-ctr -pbkdf2 -pass pass:$((SEED * i)) -nosalt < /dev/zero 2>/dev/null))
@@ -61,8 +61,8 @@ for r in $(seq 1 $((N_REPEATED_EXPERIMENTS))); do
 
             AGENT_ID="PIPUCK$i"
 
-            sed -i "s/{{${AGENT_ID}_ORIENTATION}}/${ORIENTATION}/" "temp_${CONFIG_FILE%.yaml}_${EXPERIMENT}"
-            echo "Agent $AGENT_ID orientation: $ORIENTATION"
+            sed -i "s/{{${AGENT_ID}_ORIENTATION}}/${ORIENTATION}/" "temp_${CONFIG_FILE%.yaml}_${EXPERIMENT}_S${SEED}"
+#            echo "Agent $AGENT_ID orientation: $ORIENTATION"
           done
 
 
@@ -74,7 +74,7 @@ for r in $(seq 1 $((N_REPEATED_EXPERIMENTS))); do
               # Remove agents from the experiment file
               if [ $remove_agents -ne 0 ]; then
                   for i in $(seq 1 $((remove_agents))); do
-                    sed -i "/<pipuck id=\"pipuck$((N_AGENTS-i+1))\"/,/<\/pipuck>/d" "temp_${CONFIG_FILE%.yaml}_${EXPERIMENT}"
+                    sed -i "/<pipuck id=\"pipuck$((N_AGENTS-i+1))\"/,/<\/pipuck>/d" "temp_${CONFIG_FILE%.yaml}_${EXPERIMENT}_S${SEED}"
                   done
               fi
 
@@ -94,10 +94,12 @@ for r in $(seq 1 $((N_REPEATED_EXPERIMENTS))); do
                 done
                 export AVERAGE_INTER_SPAWN_TIME
 
-                METRIC_PATH="experiment_results/${EXPERIMENT%.argos}/${CONFIG_FILE%.yaml}/spawn_time_${AVERAGE_INTER_SPAWN_TIME}/${REMAINING_AGENTS}_agents"
+                METRIC_PATH="experiment_results/${EXPERIMENT%.argos}/${CONFIG_FILE%.yaml}/spawn_time_${AVERAGE_INTER_SPAWN_TIME}/${REMAINING_AGENTS}_agents/S${SEED}"
+                mkdir -p "$METRIC_PATH"
                 export METRIC_PATH
-                FRAME_PATH="experiment_results/${EXPERIMENT%.argos}/${CONFIG_FILE%.yaml}/spawn_time_${AVERAGE_INTER_SPAWN_TIME}/${REMAINING_AGENTS}_agents/frames"
-                sed "s|{{FRAME_PATH}}|${FRAME_PATH}|g" "temp_${CONFIG_FILE%.yaml}_${EXPERIMENT}" > "temp_${CONFIG_FILE%.yaml}_${REMAINING_AGENTS}_${AVERAGE_INTER_SPAWN_TIME}_${EXPERIMENT}"
+
+                FRAME_PATH="experiment_results/${EXPERIMENT%.argos}/${CONFIG_FILE%.yaml}/spawn_time_${AVERAGE_INTER_SPAWN_TIME}/${REMAINING_AGENTS}_agents/S${SEED}/frames"
+                sed "s|{{FRAME_PATH}}|${FRAME_PATH}|g" "temp_${CONFIG_FILE%.yaml}_${EXPERIMENT}_S${SEED}" > "temp_${CONFIG_FILE%.yaml}_${REMAINING_AGENTS}_${AVERAGE_INTER_SPAWN_TIME}_${EXPERIMENT}_S${SEED}"
 
                 mkdir -p "$FRAME_PATH"
                 # empty the directory
@@ -106,19 +108,16 @@ for r in $(seq 1 $((N_REPEATED_EXPERIMENTS))); do
                 # Generate timestamp for log file
                 TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
   #              LOGFILE="$LOG_DIR/${EXPERIMENT%.argos}_${CONFIG_FILE%.yaml}_spawn_time_${AVERAGE_INTER_SPAWN_TIME}_${REMAINING_AGENTS}_agents_$TIMESTAMP.log"
-                LOGFILE="$METRIC_PATH/${EXPERIMENT%.argos}_${CONFIG_FILE%.yaml}_spawn_time_${AVERAGE_INTER_SPAWN_TIME}_${REMAINING_AGENTS}_agents_$TIMESTAMP.log"
-                echo "Running ARGoS3 with configuration: $EXP_PATH and $CONFIG_PATH with inter_spawn_time $AVERAGE_INTER_SPAWN_TIME with ${REMAINING_AGENTS} agents"
+                LOGFILE="$METRIC_PATH/${EXPERIMENT%.argos}_S${SEED}_${CONFIG_FILE%.yaml}_spawn_time_${AVERAGE_INTER_SPAWN_TIME}_${REMAINING_AGENTS}_agents_$TIMESTAMP.log"
+                echo "Running ARGoS3 with configuration: S${SEED}, $EXP_PATH and $CONFIG_PATH with inter_spawn_time $AVERAGE_INTER_SPAWN_TIME with ${REMAINING_AGENTS} agents"
 
 
 
                 # Run ARGoS3 and log output, and save exit code
-                "$ARGOSEXEC" -n -c "temp_${CONFIG_FILE%.yaml}_${REMAINING_AGENTS}_${AVERAGE_INTER_SPAWN_TIME}_${EXPERIMENT}" > "$LOGFILE" 2>&1 &
+                "$ARGOSEXEC" -n -c "temp_${CONFIG_FILE%.yaml}_${REMAINING_AGENTS}_${AVERAGE_INTER_SPAWN_TIME}_${EXPERIMENT}_S${SEED}" > "$LOGFILE" 2>&1 &
                 PID=$!
                 echo "ARGoS3 process $PID started"
-                pids[$PID]="$EXPERIMENT|$CONFIG_FILE|$AVERAGE_INTER_SPAWN_TIME|$REMAINING_AGENTS|$LOGFILE"
-
-                #wait 5 seconds before starting the next experiment, so it has time to initialize the exported variables
-                sleep 5
+                pids[$PID]="$SEED|$EXPERIMENT|$CONFIG_FILE|$AVERAGE_INTER_SPAWN_TIME|$REMAINING_AGENTS|$LOGFILE"
               done
         done
 
@@ -132,11 +131,11 @@ for pid in "${!pids[@]}"; do
     wait $pid
     exit_code=$?
     echo "ARGoS3 process $pid has exited with status $exit_code"
-    IFS='|' read -r EXPERIMENT CONFIG_FILE AVERAGE_INTER_SPAWN_TIME REMAINING_AGENTS LOGFILE <<< "${pids[$pid]}"
+    IFS='|' read -r SEED EXPERIMENT CONFIG_FILE AVERAGE_INTER_SPAWN_TIME REMAINING_AGENTS LOGFILE <<< "${pids[$pid]}"
     if [ $exit_code -eq 0 ]; then
-        echo "Experiment '$EXPERIMENT' with config '$CONFIG_FILE' with inter_spawn_time $AVERAGE_INTER_SPAWN_TIME and ${REMAINING_AGENTS} agents finished successfully."
+        echo "Experiment '$EXPERIMENT' with config '$CONFIG_FILE' and seed $SEED with inter_spawn_time $AVERAGE_INTER_SPAWN_TIME and ${REMAINING_AGENTS} agents finished successfully."
     else
-        echo "Experiment '$EXPERIMENT' with config '$CONFIG_FILE' with inter_spawn_time $AVERAGE_INTER_SPAWN_TIME and ${REMAINING_AGENTS} agents failed. Check log: $LOGFILE" >&2
+        echo "Experiment '$EXPERIMENT' with config '$CONFIG_FILE' and seed $SEED with inter_spawn_time $AVERAGE_INTER_SPAWN_TIME and ${REMAINING_AGENTS} agents failed. Check log: $LOGFILE" >&2
     fi
 done
 
