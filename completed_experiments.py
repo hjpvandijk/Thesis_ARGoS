a635556b0969cb44b66d33b23fb9da81e13ac90b
hjpvandijk <hugojpvandijk@gmail.com>
2025-03-11 23:36:12 +0100

    completed experiments script and files

--- completed_experiments.py
+++ completed_experiments.py
@@ -4,38 +4,43 @@ import pandas as pd
 
 
 def check_certainty_csv(root_dir):
-    certainty_files = []
+    certainty_files = {}
     non_completed_experiments = 0
+    n_completed_experiments = 0
 
     # Check if the root directory exists
     if not os.path.exists(root_dir):
-        # print('The root directory does not exist.')
-        #throw an exception
-        Exception('The root directory does not exist.')
+        raise Exception('The root directory does not exist.')
 
     # Walk through all directories and subdirectories
     for root, dirs, files in os.walk(root_dir):
-        # print('Checking directory:', root)
         # Check if 'certainty.csv' exists in the current directory
         if 'certainty.csv' in files:
             file_path = os.path.join(root, 'certainty.csv')
             # Check if the file is not empty
-            if pd.read_csv(file_path).empty == False:
-                #remove 'implementation_and_examples/' from the file path
+            if not pd.read_csv(file_path).empty:
+                # Extract the most outer directory name after the root dir
+                outer_dir = root.split('/')[2]
+                # Initialize the list if the key does not exist
+                if outer_dir not in certainty_files:
+                    certainty_files[outer_dir] = []
+                # Remove 'implementation_and_examples/' from the file path
                 file_path = file_path.replace('implementation_and_examples/', '')
-                #remove 'certainty.csv' from the file path
+                # Remove 'certainty.csv' from the file path
                 file_path = file_path.replace('/certainty.csv', '')
-                certainty_files.append(file_path)
+                certainty_files[outer_dir].append(file_path)
+                n_completed_experiments += 1
             else:
                 print('The file is empty:', file_path)
         else:
-            #if the last dir doesn't start with 'S', skip
+            # If the last dir doesn't start with 'S', skip
             if root.split('/')[-1][0] != 'S':
                 continue
             print('The file does not exist:', root)
             non_completed_experiments += 1
 
     print('Number of non-completed experiments:', non_completed_experiments)
+    print('Number of completed experiments:', n_completed_experiments)
     return certainty_files
 
 # Replace with the directory path you want to search
@@ -43,7 +48,9 @@ root_directory = 'implementation_and_examples/experiment_results'
 certainty_files = check_certainty_csv(root_directory)
 
 # export the list of certainty files to a csv file
-with open('completed_experiments.csv', 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile)
-    for file in certainty_files:
-        writer.writerow([file])
+for outer_dir, files in certainty_files.items():
+    with open('completed_experiments_' + outer_dir +'.csv', 'w', newline='\n') as csvfile:
+        writer = csv.writer(csvfile)
+        for file in files:
+            writer.writerow([file])
+
