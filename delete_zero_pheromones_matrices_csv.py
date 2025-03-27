068ae077ca4864231ac8015803f9fdbf063eba0c
hjpvandijk <hugojpvandijk@gmail.com>
2025-03-27 01:45:00 +0100

    delete matrices zero pheromones

--- delete_zero_pheromones_matrices_csv.py
+++ delete_zero_pheromones_matrices_csv.py
@@ -1,23 +1,46 @@
 import pandas as pd
 import argparse
+import os
 
-def filter_csv(input_file, output_file, column_name):
+def find_matrix_files(directory):
+    matrix_files = []
+    for root, _, files in os.walk(directory):
+        for file in files:
+            if file.startswith("coverage_matrix") or file.startswith("obstacle_matrix"):
+                filepath = os.path.join(root, file)
+                filter_csv(filepath)
+
+
+
+def filter_csv(input_file):
+    if "coverage_matrix" in input_file:
+        column_name = "coverage_pheromone"
+    elif "obstacle_matrix" in input_file:
+        column_name = "obstacle_pheromone"
+    else:
+        print("Unknown file type.")
+        return
+    
     # Read the CSV file
     df = pd.read_csv(input_file)
     
     # Filter out rows where the specified column has value 0
     df_filtered = df[df[column_name] != 0]
     
+    # Set output filename
+    #output_file = input_file.replace(".csv", "_nonzero.csv")
+    
     # Save the modified CSV
-    df_filtered.to_csv(output_file, index=False)
-    print(f"Filtered CSV saved as {output_file}")
+    df_filtered.to_csv(input_file, index=False)
+    #print(f"Filtered CSV saved as {input_file}")
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Remove rows with 0 in a specified column from a CSV file.")
-    parser.add_argument("input_file", help="Path to the input CSV file")
-    parser.add_argument("output_file", help="Path to save the modified CSV file")
-    parser.add_argument("column_name", help="Column to check for 0 values")
+filename = "implementation_and_examples/experiment_results/office/BASE_AAVFIX_end_time_600_noise_0_wifi_range_99999_message_loss_probability_0_frontier_search_radius_5_max_frontier_regions_20_evaporation_time_100/spawn_time_0/2_agents/S5/coverage_matrix_all_done.csv"
+
+output_file = "implementation_and_examples/experiment_results/office/BASE_AAVFIX_end_time_600_noise_0_wifi_range_99999_message_loss_probability_0_frontier_search_radius_5_max_frontier_regions_20_evaporation_time_100/spawn_time_0/2_agents/S5/coverage_matrix_all_done_nonzero.csv"
+
+column_name = "coverage_pheromone"
+
+directory = 'implementation_and_examples/experiment_results'
+
+find_matrix_files(directory)
     
-    args = parser.parse_args()
-    
-    filter_csv(args.input_file, args.output_file, args.column_name)
