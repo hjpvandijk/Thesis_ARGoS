e9fbd28d2fce425c6a38ba2ed4a7bd8458a6ddcf
hjpvandijk <hugojpvandijk@gmail.com>
2025-04-17 23:56:07 +0200

    noise bigger Rm

--- implementation_and_examples/run_experiment_batch2.sh
+++ implementation_and_examples/run_experiment_batch2.sh
@@ -15,19 +15,19 @@ ARGOSEXEC="argos3"
 mkdir -p "$LOG_DIR"
 
 # List of experiment files (modify as needed)
-#EXPERIMENTS=("house.argos" "house_tilted.argos" "office.argos" "office_tilted.argos" "museum.argos" "museum_tilted.argos")
-EXPERIMENTS=("house.argos" "house_tilted.argos" "office.argos" "office_tilted.argos")
+EXPERIMENTS=("house.argos" "house_tilted.argos" "office.argos" "office_tilted.argos" "museum.argos" "museum_tilted.argos")
+#EXPERIMENTS=("house.argos" "house_tilted.argos" "office.argos" "office_tilted.argos")
 #EXPERIMENTS=("museum_tilted.argos")
 #CONFIGS=("config__alignment0_1__cohesion__0.yaml" "config__alignment0_1__cohesion__0_1.yaml" "config__alignment0__cohesion__0.yaml" "config__alignment0__cohesion__0_1.yaml")
 #CONFIGS=("config_bigger_safety_n_1.yaml" "config_bigger_safety_range.yaml" "config_bigger_safety_n_3.yaml")
 #CONFIGS=("n_3_m_2_5_cellratio0_75_noise.yaml" "n_3_m_2_5_cellratio0_75_noise_agent_avoidance_0_5.yaml" "n_3_m_2_5_cellratio0_75_noise_object_safety_0_3.yaml")
 #CONFIGS=("p_sensor_1.yaml")
 CONFIGS=(
-"AAVFIX_end_time_{END_TIME}_noise_0_wifi_range_99999_message_loss_probability_0_frontier_search_radius_99999_max_frontier_regions_99999_evaporation_time_100_max_route_length_99999.yaml"
-"AAVFIX_end_time_{END_TIME}_noise_0_5_wifi_range_99999_message_loss_probability_0_frontier_search_radius_99999_max_frontier_regions_99999_evaporation_time_100_max_route_length_99999.yaml"
-"AAVFIX_end_time_{END_TIME}_noise_1_wifi_range_99999_message_loss_probability_0_frontier_search_radius_99999_max_frontier_regions_99999_evaporation_time_100_max_route_length_99999.yaml"
-"AAVFIX_end_time_{END_TIME}_noise_1_5_wifi_range_99999_message_loss_probability_0_frontier_search_radius_99999_max_frontier_regions_99999_evaporation_time_100_max_route_length_99999.yaml"
-)
+        "AAVFIXoorm0o3_end_time_{END_TIME}_noise_0_wifi_range_99999_message_loss_probability_0_frontier_search_radius_99999_max_frontier_regions_99999_evaporation_time_100_max_route_length_99999.yaml"
+        "AAVFIXoorm0o3_end_time_{END_TIME}_noise_0_5_wifi_range_99999_message_loss_probability_0_frontier_search_radius_99999_max_frontier_regions_99999_evaporation_time_100_max_route_length_99999.yaml"
+        "AAVFIXoorm0o3_end_time_{END_TIME}_noise_1_wifi_range_99999_message_loss_probability_0_frontier_search_radius_99999_max_frontier_regions_99999_evaporation_time_100_max_route_length_99999.yaml"
+        "AAVFIXoorm0o3_end_time_{END_TIME}_noise_1_5_wifi_range_99999_message_loss_probability_0_frontier_search_radius_99999_max_frontier_regions_99999_evaporation_time_100_max_route_length_99999.yaml"
+        )
 
 
 
